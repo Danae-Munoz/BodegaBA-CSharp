4e33c0b029e17521dc673e8828f19ca227a16de3
Danae21 <136654165+Danae-Munoz@users.noreply.github.com>
2025-05-19 20:25:01 -0400

    Subido

---
+++ BuenosAires.BodegaBA/VentanaLogin.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace BuenosAires.BodegaBA
+{
+    partial class VentanaLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCuenta = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.lblCuenta = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblTituloVentana = new System.Windows.Forms.Label();
+            this.lblNombreSistema = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtCuenta
+            // 
+            this.txtCuenta.Location = new System.Drawing.Point(184, 114);
+            this.txtCuenta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCuenta.Name = "txtCuenta";
+            this.txtCuenta.Size = new System.Drawing.Size(132, 22);
+            this.txtCuenta.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(184, 160);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(132, 22);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(107, 213);
+            this.btnIngresar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(191, 28);
+            this.btnIngresar.TabIndex = 2;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // lblCuenta
+            // 
+            this.lblCuenta.AutoSize = true;
+            this.lblCuenta.Location = new System.Drawing.Point(84, 118);
+            this.lblCuenta.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCuenta.Name = "lblCuenta";
+            this.lblCuenta.Size = new System.Drawing.Size(49, 16);
+            this.lblCuenta.TabIndex = 4;
+            this.lblCuenta.Text = "Cuenta";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(84, 164);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(76, 16);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Contraseña";
+            // 
+            // lblTituloVentana
+            // 
+            this.lblTituloVentana.AutoSize = true;
+            this.lblTituloVentana.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloVentana.Location = new System.Drawing.Point(47, 58);
+            this.lblTituloVentana.Name = "lblTituloVentana";
+            this.lblTituloVentana.Size = new System.Drawing.Size(308, 25);
+            this.lblTituloVentana.TabIndex = 7;
+            this.lblTituloVentana.Text = "Ingresar al Sistema de Bodega";
+            // 
+            // lblNombreSistema
+            // 
+            this.lblNombreSistema.AutoSize = true;
+            this.lblNombreSistema.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreSistema.Location = new System.Drawing.Point(83, 11);
+            this.lblNombreSistema.Name = "lblNombreSistema";
+            this.lblNombreSistema.Size = new System.Drawing.Size(225, 25);
+            this.lblNombreSistema.TabIndex = 8;
+            this.lblNombreSistema.Text = "Sistema Buenos Aires";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(331, 218);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(50, 20);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "123";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // VentanaLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 268);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lblNombreSistema);
+            this.Controls.Add(this.lblTituloVentana);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblCuenta);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtCuenta);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "VentanaLogin";
+            this.Text = "Iniciar sesión";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCuenta;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label lblCuenta;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblTituloVentana;
+        private System.Windows.Forms.Label lblNombreSistema;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
---
+++ BuenosAires.BodegaBA/VentanaLogin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using BuenosAires.Model.Utiles;
+using BuenosAires.ServiceProxy;
+
+namespace BuenosAires.BodegaBA
+{
+    public partial class VentanaLogin : Form
+    {
+        public VentanaLogin()
+        {
+            InitializeComponent();
+            this.AcceptButton = btnIngresar;
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            var sc = new ScAutenticacion();
+            sc.Autenticar("Bodeguero", txtCuenta.Text, txtPassword.Text);
+            if (sc.Autenticado)
+            {
+                new VentanaProducto(sc.NombreUsuario + " (" + sc.TipoUsuario + ")").Show();
+                Hide();
+            }
+            else
+            {
+                this.MensajeInfo(sc.Mensaje);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
